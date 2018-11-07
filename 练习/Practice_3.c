c99678c59fb05cbc76c237aa91651c5fb1c0c71b
nubiaX <872408568@qq.com>
2018-11-07 22:58:43 +0800

    提交代码

---
+++ 练习/Practice_3.c
@@ -0,0 +1,16 @@
+//判断素数
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{	
+
+	
+	int n = 3,i = 2;
+	printf("请输入一个数：");
+	sancf("%d", &i);
+	system("pause");
+	return 0;
+}
